5badf8714c125bb53dcaeff84b0424f081e30380
Demi Plessas <demitri.plessas@gmail.com>
2024-12-25 20:49:06 -0800

    Adding flash 2.0 thinking experimental

--- lofn/llm_integration.py
+++ lofn/llm_integration.py
@@ -496,6 +496,7 @@ def get_llm(model, temperature, OPENAI_API=None, ANTHROPIC_API=None, debug=False
 
             # Google models
             "gemini-2.0-flash-exp": 8191,
+            "gemini-2.0-flash-thinking-exp": 32768,
             "gemini-1.5-flash": 16384,
             "gemini-1.5-flash-002": 8191,
             "gemini-1.5-pro": 32768,
--- lofn/ui.py
+++ lofn/ui.py
@@ -54,7 +54,7 @@ class LofnApp:
         # Add Google models if GOOGLE_API is available
         if Config.GOOGLE_API:
             models.extend([
-                "gemini-2.0-flash-exp", "gemini-1.5-flash-002", "gemini-1.5-pro-002",
+                "gemini-2.0-flash-exp", "gemini-2.0-flash-thinking-exp", "gemini-1.5-flash-002", "gemini-1.5-pro-002",
                 "gemini-exp-1206", "gemini-exp-1121", "gemini-exp-1114",
                 "gemini-1.5-flash", "gemini-1.5-pro", "gemini-1.0-pro",
                 "gemini-1.5-pro-exp-0827", "gemini-1.5-pro-exp-0801"
